5f5c27e36e0a7b8329ec8cd208502ced234c753a
semucham <semu.newPC@outlook.jp>
2021-01-07 14:46:38 +0900

    Iわまつサーバ適応型

--- WebApplication1/WebApplication1/SQL_oparations.cs
+++ WebApplication1/WebApplication1/SQL_oparations.cs
@@ -11,10 +11,10 @@ namespace WebApplication1
 		public static void conections()
         {
 			// MySQLへの接続情報
-			string server = "localhost";        // MySQLサーバホスト名
-			string user = "root";           // MySQLユーザ名
-			string pass = "";           // MySQLパスワード
-			string database = "mysql";      // 接続するデータベース名
+			string server = "us-cdbr-east-02.cleardb.com";        // MySQLサーバホスト名
+			string user = "bcc8a0e09211c7";           // MySQLユーザ名
+			string pass = "f783a8d5";           // MySQLパスワード
+			string database = "heroku_3c74537ac26405b";      // 接続するデータベース名
 			string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
 			// MySQLへの接続
 			try
@@ -29,6 +29,8 @@ namespace WebApplication1
 			{
 				Console.WriteLine("ERROR: " + me.Message);
 			}
+
+
 		}
     }
 }
