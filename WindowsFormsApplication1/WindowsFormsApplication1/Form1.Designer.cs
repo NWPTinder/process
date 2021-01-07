fc37b937a79b029b5f81cae9ea6cbf11aa354cff
tmtm141 <tomoppironosuke@gmail.com>
2021-01-07 15:38:33 +0900

    a

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Thumbup_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -56,7 +56,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(34, 62);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(34, 12);
             this.label5.TabIndex = 9;
             this.label5.Text = "Name";
             // 
@@ -64,14 +64,14 @@
             // 
             this.textBox5.Location = new System.Drawing.Point(36, 77);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(227, 20);
+            this.textBox5.Size = new System.Drawing.Size(227, 19);
             this.textBox5.TabIndex = 10;
             // 
             // textBox6
             // 
             this.textBox6.Location = new System.Drawing.Point(299, 77);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(27, 20);
+            this.textBox6.Size = new System.Drawing.Size(27, 19);
             this.textBox6.TabIndex = 11;
             // 
             // label6
@@ -79,7 +79,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(297, 62);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.Size = new System.Drawing.Size(25, 12);
             this.label6.TabIndex = 12;
             this.label6.Text = "Age";
             // 
@@ -96,7 +96,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(34, 112);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.Size = new System.Drawing.Size(63, 12);
             this.label7.TabIndex = 14;
             this.label7.Text = "Description";
             // 
@@ -105,7 +105,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(658, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 13);
+            this.label2.Size = new System.Drawing.Size(136, 12);
             this.label2.TabIndex = 16;
             this.label2.Text = "新規登録・プロフィール更新";
             // 
@@ -159,15 +159,15 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button3
+            // Thumbup_button
             // 
-            this.button3.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.good2;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(450, 198);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(115, 113);
-            this.button3.TabIndex = 6;
-            this.button3.UseVisualStyleBackColor = true;
+            this.Thumbup_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.good2;
+            this.Thumbup_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Thumbup_button.Location = new System.Drawing.Point(450, 198);
+            this.Thumbup_button.Name = "Thumbup_button";
+            this.Thumbup_button.Size = new System.Drawing.Size(115, 113);
+            this.Thumbup_button.TabIndex = 6;
+            this.Thumbup_button.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -182,7 +182,7 @@
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Thumbup_button);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -193,7 +193,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Thumbup_button;
         private System.Windows.Forms.Button button4;
         private System.Diagnostics.EventLog eventLog1;
         private System.Windows.Forms.Label label5;
