f392fdbc354bbf798419f8725ce9d5b867df19bc
semucham <semu.newPC@outlook.jp>
2021-01-07 14:39:00 +0900

    conection 完成

--- WebApplication1/WebApplication1/SQL_oparations.cs
+++ WebApplication1/WebApplication1/SQL_oparations.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
 
 namespace WebApplication1
 {
-    public class SQL_oparations
-    {
+	public class SQL_oparations
+	{
+		public static void conections()
+        {
+			// MySQLへの接続情報
+			string server = "localhost";        // MySQLサーバホスト名
+			string user = "root";           // MySQLユーザ名
+			string pass = "";           // MySQLパスワード
+			string database = "mysql";      // 接続するデータベース名
+			string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+			// MySQLへの接続
+			try
+			{
+				MySqlConnection connection = new MySqlConnection(connectionString);
+				connection.Open();
+				Console.WriteLine("MySQLに接続しました！");
+				// 接続の解除
+				connection.Close();
+			}
+			catch (MySqlException me)
+			{
+				Console.WriteLine("ERROR: " + me.Message);
+			}
+		}
     }
 }
