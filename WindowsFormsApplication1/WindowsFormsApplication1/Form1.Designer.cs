d56ad7e698a1197a2aa397cbabb6eed5980a057d
493n <0mtakuma1018@gmail.com>
2021-01-25 21:40:08 +0900

    配置とフォントの変更、いいねボタンの画像の置き換え 問題があったら戻してください

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -37,12 +37,12 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView_Ranking = new System.Windows.Forms.DataGridView();
-            this.Registration_button = new System.Windows.Forms.Button();
-            this.Update_button = new System.Windows.Forms.Button();
-            this.Thumbup_button = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.ID_display = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.Registration_button = new System.Windows.Forms.Button();
+            this.Update_button = new System.Windows.Forms.Button();
+            this.Thumbup_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Ranking)).BeginInit();
             this.SuspendLayout();
@@ -54,41 +54,46 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(34, 62);
+            this.label5.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(32, 50);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.Size = new System.Drawing.Size(42, 24);
             this.label5.TabIndex = 9;
             this.label5.Text = "名前";
             // 
             // Name_display
             // 
+            this.Name_display.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Name_display.Location = new System.Drawing.Point(36, 77);
             this.Name_display.Name = "Name_display";
-            this.Name_display.Size = new System.Drawing.Size(227, 19);
+            this.Name_display.Size = new System.Drawing.Size(227, 31);
             this.Name_display.TabIndex = 10;
             this.Name_display.TextChanged += new System.EventHandler(this.Name_display_TextChanged);
             // 
             // Age_display
             // 
-            this.Age_display.Location = new System.Drawing.Point(299, 77);
+            this.Age_display.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Age_display.Location = new System.Drawing.Point(323, 77);
             this.Age_display.Name = "Age_display";
-            this.Age_display.Size = new System.Drawing.Size(27, 19);
+            this.Age_display.Size = new System.Drawing.Size(38, 31);
             this.Age_display.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(297, 62);
+            this.label6.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(319, 50);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.Size = new System.Drawing.Size(42, 24);
             this.label6.TabIndex = 12;
             this.label6.Text = "年齢";
             // 
             // Description_display
             // 
-            this.Description_display.Location = new System.Drawing.Point(36, 127);
+            this.Description_display.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Description_display.Location = new System.Drawing.Point(36, 156);
             this.Description_display.Name = "Description_display";
-            this.Description_display.Size = new System.Drawing.Size(398, 78);
+            this.Description_display.Size = new System.Drawing.Size(325, 95);
             this.Description_display.TabIndex = 13;
             this.Description_display.Text = "";
             this.Description_display.TextChanged += new System.EventHandler(this.Description_display_TextChanged);
@@ -96,64 +101,33 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(34, 112);
+            this.label7.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(32, 129);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.Size = new System.Drawing.Size(74, 24);
             this.label7.TabIndex = 14;
             this.label7.Text = "自己紹介";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(606, 32);
+            this.label2.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(576, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 12);
+            this.label2.Size = new System.Drawing.Size(218, 24);
             this.label2.TabIndex = 16;
             this.label2.Text = "新規登録・プロフィール更新";
             // 
             // dataGridView_Ranking
             // 
             this.dataGridView_Ranking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_Ranking.Location = new System.Drawing.Point(587, 183);
+            this.dataGridView_Ranking.Location = new System.Drawing.Point(450, 192);
             this.dataGridView_Ranking.Name = "dataGridView_Ranking";
             this.dataGridView_Ranking.RowHeadersWidth = 51;
             this.dataGridView_Ranking.RowTemplate.Height = 21;
             this.dataGridView_Ranking.Size = new System.Drawing.Size(344, 276);
             this.dataGridView_Ranking.TabIndex = 17;
             this.dataGridView_Ranking.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Ranking_CellContentClick);
-            // 
-            // Registration_button
-            // 
-            this.Registration_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.touroku1;
-            this.Registration_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Registration_button.Location = new System.Drawing.Point(596, 62);
-            this.Registration_button.Name = "Registration_button";
-            this.Registration_button.Size = new System.Drawing.Size(132, 99);
-            this.Registration_button.TabIndex = 15;
-            this.Registration_button.UseVisualStyleBackColor = true;
-            this.Registration_button.Click += new System.EventHandler(this.Registration_button_Click);
-            // 
-            // Update_button
-            // 
-            this.Update_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.reload1;
-            this.Update_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Update_button.Location = new System.Drawing.Point(450, 344);
-            this.Update_button.Name = "Update_button";
-            this.Update_button.Size = new System.Drawing.Size(115, 115);
-            this.Update_button.TabIndex = 7;
-            this.Update_button.UseVisualStyleBackColor = true;
-            this.Update_button.Click += new System.EventHandler(this.Update_button_Click);
-            // 
-            // Thumbup_button
-            // 
-            this.Thumbup_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.good2;
-            this.Thumbup_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Thumbup_button.Location = new System.Drawing.Point(450, 198);
-            this.Thumbup_button.Name = "Thumbup_button";
-            this.Thumbup_button.Size = new System.Drawing.Size(115, 113);
-            this.Thumbup_button.TabIndex = 6;
-            this.Thumbup_button.UseVisualStyleBackColor = true;
-            this.Thumbup_button.Click += new System.EventHandler(this.Thumbup_button_Click);
             // 
             // label1
             // 
@@ -176,16 +150,49 @@
             // 
             this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.reload1;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(794, 62);
+            this.button1.Location = new System.Drawing.Point(714, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 99);
+            this.button1.Size = new System.Drawing.Size(80, 80);
             this.button1.TabIndex = 21;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Registration_button
+            // 
+            this.Registration_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.new_user;
+            this.Registration_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Registration_button.Location = new System.Drawing.Point(580, 77);
+            this.Registration_button.Name = "Registration_button";
+            this.Registration_button.Size = new System.Drawing.Size(80, 80);
+            this.Registration_button.TabIndex = 15;
+            this.Registration_button.UseVisualStyleBackColor = true;
+            this.Registration_button.Click += new System.EventHandler(this.Registration_button_Click);
+            // 
+            // Update_button
+            // 
+            this.Update_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.reload1;
+            this.Update_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Update_button.Location = new System.Drawing.Point(251, 257);
+            this.Update_button.Name = "Update_button";
+            this.Update_button.Size = new System.Drawing.Size(110, 110);
+            this.Update_button.TabIndex = 7;
+            this.Update_button.UseVisualStyleBackColor = true;
+            this.Update_button.Click += new System.EventHandler(this.Update_button_Click);
+            // 
+            // Thumbup_button
+            // 
+            this.Thumbup_button.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.iine;
+            this.Thumbup_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Thumbup_button.Location = new System.Drawing.Point(36, 257);
+            this.Thumbup_button.Name = "Thumbup_button";
+            this.Thumbup_button.Size = new System.Drawing.Size(110, 110);
+            this.Thumbup_button.TabIndex = 6;
+            this.Thumbup_button.UseVisualStyleBackColor = true;
+            this.Thumbup_button.Click += new System.EventHandler(this.Thumbup_button_Click);
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(943, 496);
+            this.ClientSize = new System.Drawing.Size(833, 496);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ID_display);
             this.Controls.Add(this.label1);
--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.Designer.cs
@@ -43,42 +43,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(35, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "名前";
             // 
             // Enter_name
             // 
-            this.Enter_name.Location = new System.Drawing.Point(14, 48);
+            this.Enter_name.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Enter_name.Location = new System.Drawing.Point(14, 44);
             this.Enter_name.Name = "Enter_name";
-            this.Enter_name.Size = new System.Drawing.Size(100, 20);
+            this.Enter_name.Size = new System.Drawing.Size(100, 27);
             this.Enter_name.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 31);
+            this.label2.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(129, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(35, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "年齢";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.Size = new System.Drawing.Size(61, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "自己紹介";
             // 
             // Completion_button
             // 
-            this.Completion_button.Location = new System.Drawing.Point(197, 267);
+            this.Completion_button.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Completion_button.Location = new System.Drawing.Point(197, 246);
             this.Completion_button.Name = "Completion_button";
-            this.Completion_button.Size = new System.Drawing.Size(75, 25);
+            this.Completion_button.Size = new System.Drawing.Size(75, 23);
             this.Completion_button.TabIndex = 6;
             this.Completion_button.Text = "登録完了";
             this.Completion_button.UseVisualStyleBackColor = true;
@@ -86,24 +91,27 @@
             // 
             // Enter_age
             // 
-            this.Enter_age.Location = new System.Drawing.Point(133, 49);
+            this.Enter_age.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Enter_age.Location = new System.Drawing.Point(133, 45);
             this.Enter_age.Name = "Enter_age";
-            this.Enter_age.Size = new System.Drawing.Size(46, 20);
+            this.Enter_age.Size = new System.Drawing.Size(46, 27);
             this.Enter_age.TabIndex = 7;
             // 
             // Enter_description
             // 
-            this.Enter_description.Location = new System.Drawing.Point(15, 88);
+            this.Enter_description.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Enter_description.Location = new System.Drawing.Point(14, 109);
             this.Enter_description.Name = "Enter_description";
-            this.Enter_description.Size = new System.Drawing.Size(100, 20);
+            this.Enter_description.Size = new System.Drawing.Size(165, 27);
             this.Enter_description.TabIndex = 8;
             // 
             // Select_male
             // 
             this.Select_male.AutoSize = true;
-            this.Select_male.Location = new System.Drawing.Point(18, 142);
+            this.Select_male.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Select_male.Location = new System.Drawing.Point(14, 165);
             this.Select_male.Name = "Select_male";
-            this.Select_male.Size = new System.Drawing.Size(49, 17);
+            this.Select_male.Size = new System.Drawing.Size(53, 24);
             this.Select_male.TabIndex = 12;
             this.Select_male.TabStop = true;
             this.Select_male.Text = "男性";
@@ -112,9 +120,10 @@
             // Select_female
             // 
             this.Select_female.AutoSize = true;
-            this.Select_female.Location = new System.Drawing.Point(90, 142);
+            this.Select_female.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Select_female.Location = new System.Drawing.Point(79, 165);
             this.Select_female.Name = "Select_female";
-            this.Select_female.Size = new System.Drawing.Size(49, 17);
+            this.Select_female.Size = new System.Drawing.Size(53, 24);
             this.Select_female.TabIndex = 13;
             this.Select_female.TabStop = true;
             this.Select_female.Text = "女性";
@@ -122,9 +131,9 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 307);
+            this.ClientSize = new System.Drawing.Size(283, 283);
             this.Controls.Add(this.Select_female);
             this.Controls.Add(this.Select_male);
             this.Controls.Add(this.Enter_description);
